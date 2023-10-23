b5c273dce56b2bef470650043d3ada79596f62da
Flynn Joffray <flynn.joffray@verifone.com>
2023-10-22 17:03:53 -0700

    initial commit
    Signed-off-by: Flynn Joffray <flynn.joffray@verifone.com>

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM balenalib/armv7hf-node:18-buster-run
+
+COPY package*.json ./
+
+RUN JOBS=MAX npm install --production --unsafe-perm && npm cache verify && rm -rf /tmp/*
+
+COPY . ./
+
+CMD ["npm", "start"]
---
+++ src/index.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const Airtable = require('airtable')
+const _ = require('lodash')
+
+const port = process.env.PORT || 3000
+
+// airtable
+Airtable.configure({
+    apiKey: process.env.AIRTABLE_TOKEN
+})
+
+var base = Airtable.base(process.env.AIRTABLE_BASE)
+
+// express
+app.use(express.static(
+    path.join(__dirname, 'public')
+))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/inventory', async (req, res) => {
+    let items = await getItems()
+    console.log('ITEMS', items)
+    let grouped = _.groupBy(items, 'Category')
+    res.json(grouped)
+})
+
+app.get('/categories', async (req, res) => {
+    let items = await getItems()
+    console.log('ITEMS', items)
+    let grouped = _.groupBy(items, 'Category')
+    res.render('categories', {
+        cats: grouped
+    })
+})
+
+app.listen(port, () => {
+  console.log(`* started on port: ${port}`)
+})
+
+// const CloverClient = CloverRestful.client('https://apisandbox.dev.clover.com')
+// const cloverToken = process.env.CLOVER_TOKEN
+// const merchantId = process.env.CLOVER_MERCHANT
+
+async function getItems() {
+    return new Promise((resolve, reject) => {
+        base('menu-signage').select({
+            view: 'menu'
+        }).firstPage((err, records) => {
+            if (err) {
+                console.error(err)
+                reject(err)
+                return
+            }
+            try {
+                let data = []
+                // console.log(records)
+                records.forEach(function(record) {
+                    console.log('Retrieved', record.fields)
+                    data.push(record.fields)
+                })
+                resolve(data)
+            } catch (err) {
+                console.log(err)
+            }        
+        })  
+    })
+}
